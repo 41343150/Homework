ad9c2119dc7e3c8352e50ca9e51bf9bbef8032e9
41343150 <qqwoplay@gmail.com>
2026-03-29 19:17:50 +0800

    Refactor Heap class to use array instead of vector

--- homework4/src/hw4_1.cpp
+++ homework4/src/hw4_1.cpp
@@ -1,69 +1,75 @@
+#include <algorithm>
 #include <iostream>
-#include <vector>
-#include <stdexcept>
 using namespace std;
 
-//////////////////////////////////////////////////////////////////
-// Heap 類別模板，可建構 MinHeap 或 MaxHeap
+//////////////////////////////////////////////////////////////
 template <typename T>
 class Heap {
 private:
-    vector<T> data;       // 存放堆元素
-    bool isMinHeap;       // true = MinHeap, false = MaxHeap
+    T arr[10000];   // 用陣列取代 vector
+    int sz;         // 目前元素數量
+    bool isMin;     // true = MinHeap, false = MaxHeap
 
-    // 比較函式，決定父子節點交換方向
-    bool compare(const T& a, const T& b) const {
-        return isMinHeap ? a < b : a > b;
+    bool cmp(const T& a, const T& b) const {
+        return isMin ? a < b : a > b;
     }
 
-    // 往上調整堆
-    void siftUp(int idx) {
-        while (idx > 0) {
-            int parent = (idx - 1) / 2;
-            if (compare(data[idx], data[parent])) {
-                swap(data[idx], data[parent]);
-                idx = parent;
+    void up(int i) {
+        while (i > 0) {
+            int p = (i - 1) / 2;
+            if (cmp(arr[i], arr[p])) {
+                swap(arr[i], arr[p]);
+                i = p;
             } else break;
         }
     }
 
-    // 往下調整堆
-    void siftDown(int idx) {
-        int n = data.size();
-        while (2 * idx + 1 < n) {
-            int left = 2 * idx + 1;
-            int right = 2 * idx + 2;
-            int swapIdx = left;
+    void down(int i) {
+        while (2 * i + 1 < sz) {
+            int l = 2 * i + 1;
+            int r = 2 * i + 2;
+            int t = l;
 
-            if (right < n && compare(data[right], data[left])) swapIdx = right;
+            if (r < sz && cmp(arr[r], arr[l])) t = r;
 
-            if (compare(data[swapIdx], data[idx])) {
-                swap(data[idx], data[swapIdx]);
-                idx = swapIdx;
+            if (cmp(arr[t], arr[i])) {
+                swap(arr[i], arr[t]);
+                i = t;
             } else break;
         }
     }
 
 public:
-    Heap(bool minHeap = true) : isMinHeap(minHeap) {}
-
-    bool empty() const { return data.empty(); }
-
-    const T& top() const {
-        if (empty()) throw runtime_error("Heap is empty!");
-        return data[0];
+    Heap(bool type = true) {
+        isMin = type;
+        sz = 0;
     }
 
-    void push(const T& val) {
-        data.push_back(val);
-        siftUp(data.size() - 1);
+    bool empty() const {
+        return sz == 0;
+    }
+
+    T top() const {
+        if (empty()) {
+            cout << "Heap is empty!\n";
+            return T();
+        }
+        return arr[0];
+    }
+
+    void push(T x) {
+        arr[sz++] = x;
+        up(sz - 1);
     }
 
     void pop() {
-        if (empty()) throw runtime_error("Heap is empty!");
-        swap(data[0], data.back());
-        data.pop_back();
-        if (!empty()) siftDown(0);
+        if (empty()) {
+            cout << "Heap is empty!\n";
+            return;
+        }
+        swap(arr[0], arr[sz - 1]);
+        sz--;
+        if (!empty()) down(0);
     }
 
     void build(int n) {
@@ -75,49 +81,46 @@ public:
     }
 
     void printLevels() const {
-        int n = data.size();
-        int idx = 0, level = 0;
-        while (idx < n) {
-            int nodes = 1 << level;
+        int i = 0, level = 0;
+        while (i < sz) {
+            int cnt = 1 << level;
             cout << "Level " << level << ": ";
-            for (int i = 0; i < nodes && idx < n; i++) {
-                cout << data[idx++] << " ";
+            for (int j = 0; j < cnt && i < sz; j++) {
+                cout << arr[i++] << " ";
             }
             cout << endl;
             level++;
         }
     }
 };
-//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////
 
 int main() {
     int n;
-    cout << "請輸入測資數量: ";
+    cout << "輸入測資數量: ";
     cin >> n;
 
-    // MinHeap
-    Heap<int> minHeap(true);
-    cout << "輸入 " << n << " 個數字 (MinHeap):\n";
-    minHeap.build(n);
+    Heap<int> h1(true);
+    cout << "輸入 MinHeap:\n";
+    h1.build(n);
 
     cout << "\nMinHeap:\n";
-    minHeap.printLevels();
-    cout << "最小值: " << minHeap.top() << endl;
-    minHeap.pop();
-    cout << "刪除最小值後:\n";
-    minHeap.printLevels();
+    h1.printLevels();
+    cout << "最小值: " << h1.top() << endl;
+    h1.pop();
+    cout << "刪除後:\n";
+    h1.printLevels();
 
-    // MaxHeap
-    Heap<int> maxHeap(false);
-    cout << "\n輸入 " << n << " 個數字 (MaxHeap):\n";
-    maxHeap.build(n);
+    Heap<int> h2(false);
+    cout << "\n輸入 MaxHeap:\n";
+    h2.build(n);
 
     cout << "\nMaxHeap:\n";
-    maxHeap.printLevels();
-    cout << "最大值: " << maxHeap.top() << endl;
-    maxHeap.pop();
-    cout << "刪除最大值後:\n";
-    maxHeap.printLevels();
+    h2.printLevels();
+    cout << "最大值: " << h2.top() << endl;
+    h2.pop();
+    cout << "刪除後:\n";
+    h2.printLevels();
 
     return 0;
 }
